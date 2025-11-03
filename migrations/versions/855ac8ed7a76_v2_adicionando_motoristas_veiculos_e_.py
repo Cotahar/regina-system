6021a549733e7a12abce912ba7fe19250c8727e8
Ruan <ruan.figueiredo21@gmail.com>
2025-11-03 15:12:05 -0300

    Adicionando pasta migrations que faltava

--- migrations/versions/855ac8ed7a76_v2_adicionando_motoristas_veiculos_e_.py
+++ migrations/versions/855ac8ed7a76_v2_adicionando_motoristas_veiculos_e_.py
@@ -1,8 +1,8 @@
 """V2 - Adicionando motoristas, veiculos e remetentes
 
-Revision ID: f194e970e7ab
+Revision ID: 855ac8ed7a76
 Revises: 
-Create Date: 2025-11-03 15:04:07.854991
+Create Date: 2025-11-03 15:11:49.868902
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f194e970e7ab'
+revision = '855ac8ed7a76'
 down_revision = None
 branch_labels = None
 depends_on = None
