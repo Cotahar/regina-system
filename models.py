e92eac1755dbbfb237b2de01a7129d65df79dfa0
Ruan <ruan.figueiredo21@gmail.com>
2025-11-04 12:47:32 -0300

    Corrigindo models.py (V4 - Relação de Entrega)

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 #
-# models.py (VERSÃO 3 - CORRIGINDO AMBIGUIDADE DE RELAÇÃO)
+# models.py (VERSÃO 4 - CORRIGINDO RELAÇÃO NA ENTREGA)
 #
 from database import db
 
@@ -50,14 +50,12 @@ class Cliente(db.Model):
     is_remetente = db.Column(db.Boolean, default=False, nullable=False)
     
     # Relação como Destinatário
-    # AQUI ESTÁ CORRETO: Especifica qual chave estrangeira 'Entrega' usa
     entregas_como_destinatario = db.relationship('Entrega', 
                                                  foreign_keys='Entrega.cliente_id', 
                                                  back_populates='cliente',
                                                  lazy=True)
     
     # Relação como Remetente
-    # AQUI ESTÁ CORRETO: Especifica qual chave estrangeira 'Entrega' usa
     entregas_como_remetente = db.relationship('Entrega', 
                                                foreign_keys='Entrega.remetente_id', 
                                                back_populates='remetente',
@@ -129,13 +127,15 @@ class Entrega(db.Model):
     carga = db.relationship('Carga', foreign_keys='Entrega.carga_id', back_populates='entregas')
 
     # Relação com o Destinatário
-    # <<< CORREÇÃO APLICADA AQUI: Removemos 'foreign_keys' >>>
+    # <<< CORREÇÃO APLICADA AQUI: Adicionando 'foreign_keys' de volta >>>
     cliente = db.relationship('Cliente', 
+                          foreign_keys=[cliente_id], 
                           back_populates='entregas_como_destinatario')
 
     # Relação com o Remetente
-    # <<< CORREÇÃO APLICADA AQUI: Removemos 'foreign_keys' >>>
+    # <<< CORREÇÃO APLICADA AQUI: Adicionando 'foreign_keys' de volta >>>
     remetente = db.relationship('Cliente', 
+                            foreign_keys=[remetente_id], 
                             back_populates='entregas_como_remetente')
 
     def to_dict(self):
